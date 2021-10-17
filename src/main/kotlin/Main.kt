3c7efe8d03a31534e5dba2d0a079ba6a9cffe732
Joevincent <jvincent1703@gmail.com>
2021-10-17 20:01:57 +0700

    feat: added score tracking feature
    added score tracking feature to the parent class and also added comment in code to increase readability.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,8 +7,11 @@ import exceptiontest.testReply
 import java.util.*
 
 fun main() {
+//    get user name
     println("Hi! Welcome to the game! Please input your name:")
     lateinit var nameInput: String
+
+//    Loop to make sure that user input valid name. ie. no blank name
     do {
         try {
             nameInput = readLine() ?: ""
@@ -18,14 +21,18 @@ fun main() {
         }
     } while (nameInput == "")
 
+//    Instantiate Human class and Computer class
     val player = Human(nameInput)
     val computer = Computer()
 
+//    determine if the game will continue(true) or end(false)
     var status = true
 
+//    Repetitive game
     do {
         playGame(player, computer)
         println("continue? Y/N")
+//        Loop to make sure use input valid entry. ie. no blank input and input value must be found in choices(map) key.
         do {
             try {
                 val reply = readLine() ?: ""
@@ -38,6 +45,11 @@ fun main() {
             }
         } while (true)
     } while (status)
+
+//    print game status upon end of game
+    player.printStatus()
+    computer.printStatus()
+
 }
 
 val choices: Map<String, String> = mapOf("1" to "Rock", "2" to "Paper", "3" to "Scissors")
@@ -49,10 +61,16 @@ fun playGame(player: Human, computer: Computer) {
     val computerMove = computer.action()
 
     if (playerMove == computerMove) {
+        player.ties++
+        computer.ties++
         println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. Tie Game")
     } else if (playerMove == "1" && computerMove == "3" || playerMove == "2" && computerMove == "1" || playerMove == "3" && computerMove == "2") {
+        player.wins++
+        computer.losses++
         println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. Congratulations! You Win")
     } else {
+        player.losses++
+        computer.wins++
         println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. You Lose")
     }
 }
--- src/main/kotlin/class/Parent.kt
+++ src/main/kotlin/class/Parent.kt
@@ -1,6 +1,15 @@
 package `class`
 
 abstract class General {
+    var wins: Int = 0
+    var losses: Int = 0
+    var ties: Int = 0
+    val numMatch: Int
+    get() = wins + losses + ties
     abstract val name: String
     abstract fun action(): String
+
+    fun printStatus() {
+        println("$name have played $numMatch match(es), with the record of $wins win(s), $losses loss(es) and $ties tie(s)")
+    }
 }
