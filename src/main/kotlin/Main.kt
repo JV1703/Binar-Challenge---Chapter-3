bcc6175382064347d9bd1c2c42193710b1e8d8ba
Joevincent <jvincent1703@gmail.com>
2021-10-17 19:36:46 +0700

    feat: added continuous play feature
    added feature to ask player if they want to continue to play the game or end the game. Remove the redundancy of inputting player name if player wishes to continue with the game.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 import `class`.Computer
 import `class`.Human
 import exceptiontest.BadName
+import exceptiontest.BadReply
 import exceptiontest.testName
+import exceptiontest.testReply
+import java.util.*
 
 fun main() {
     println("Hi! Welcome to the game! Please input your name:")
@@ -18,7 +21,23 @@ fun main() {
     val player = Human(nameInput)
     val computer = Computer()
 
-    playGame(player, computer)
+    var status = true
+
+    do {
+        playGame(player, computer)
+        println("continue? Y/N")
+        do {
+            try {
+                val reply = readLine() ?: ""
+                testReply(reply)
+                if (reply.uppercase(Locale.getDefault()) == "N") status = false
+                break
+            } catch (e: BadReply) {
+                println("Invalid input.")
+                println("Would you like to play again? Y/N")
+            }
+        } while (true)
+    } while (status)
 }
 
 val choices: Map<String, String> = mapOf("1" to "Rock", "2" to "Paper", "3" to "Scissors")
@@ -31,7 +50,7 @@ fun playGame(player: Human, computer: Computer) {
 
     if (playerMove == computerMove) {
         println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. Tie Game")
-    }else if (playerMove == "1" && computerMove == "3" || playerMove == "2" && computerMove == "1" || playerMove == "3" && computerMove == "2") {
+    } else if (playerMove == "1" && computerMove == "3" || playerMove == "2" && computerMove == "1" || playerMove == "3" && computerMove == "2") {
         println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. Congratulations! You Win")
     } else {
         println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. You Lose")
