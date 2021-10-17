0e3fd9e286b996761900685d622e364b473eeff1
Joevincent <jvincent1703@gmail.com>
2021-10-17 19:05:43 +0700

    feat: added playGame function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,23 @@ fun main() {
 
     val player = Human(nameInput)
     val computer = Computer()
+
+    playGame(player, computer)
 }
 
 val choices: Map<String, String> = mapOf("1" to "Rock", "2" to "Paper", "3" to "Scissors")
+
+
+fun playGame(player: Human, computer: Computer) {
+    var output: String = ""
+    val playerMove = player.action()
+    val computerMove = computer.action()
+
+    if (playerMove == computerMove) {
+        println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. Tie Game")
+    }else if (playerMove == "1" && computerMove == "3" || playerMove == "2" && computerMove == "1" || playerMove == "3" && computerMove == "2") {
+        println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. Congratulations! You Win")
+    } else {
+        println("${player.name} played ${choices[playerMove]} and ${computer.name} played ${choices[computerMove]}. You Lose")
+    }
+}
